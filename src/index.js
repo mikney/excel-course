f7cbe0d494c58e18ebada28d63199ef40ba275a8
Genadii <zherkovpasha@gmail.com>
2021-04-12 15:42:26 +0300

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import './module'
 import './sccs/index.scss'
-console.log('Gello webpack');
-console.log('dada');
+import {Excel} from "@/components/excel/Excel";
+import {Header} from "@/components/header/Header";
+import {Toolbar} from "@/components/toolbar/Toolbar";
+import {Formula} from "@/components/formula/Formula";
+import {Table} from "@/components/table/Table";
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
